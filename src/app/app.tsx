d316857261bfe86a97fa9e10c3b18cbc431a4308
neilsen-ngoi <112848446+neilsen-ngoi@users.noreply.github.com>
2023-09-04 11:34:37 +1200

    more cleanup

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
-import Logos from "components/atoms/logos";
-import Card from "components/organisms/card";
+import Logos from 'components/atoms/logos'
+import Card from 'components/organisms/card'
 import {
   BeakerIcon,
   BookmarkIcon,
@@ -12,129 +12,131 @@ import {
   Bars3Icon,
   PencilIcon,
   PhotoIcon,
-} from "@heroicons/react/24/outline";
-import { QuestionMarkCircleIcon } from "@heroicons/react/24/solid";
-import Button from "components/atoms/button";
-import CopyButton from "components/molecules/copy-button";
+} from '@heroicons/react/24/outline'
+import { QuestionMarkCircleIcon } from '@heroicons/react/24/solid'
+import Button from 'components/atoms/button'
+import CopyButton from 'components/molecules/copy-button'
 
-import styles from "./app.module.css";
+import styles from './app.module.css'
 
 const features = [
   {
-    name: "Vite",
+    name: 'Vite',
     description:
-      "Faster and leaner development experience for modern web projects.",
+      'Faster and leaner development experience for modern web projects.',
     logo: CubeTransparentIcon,
-    docs: "https://vitejs.dev/",
+    docs: 'https://vitejs.dev/',
   },
   {
-    name: "React",
-    description: "JavaScript library for building user interfaces.",
+    name: 'React',
+    description: 'JavaScript library for building user interfaces.',
     logo: PencilIcon,
-    docs: "https://reactjs.org/",
+    docs: 'https://reactjs.org/',
   },
   {
-    name: "TypeScript",
+    name: 'TypeScript',
     description:
-      "Strongly typed programming language that builds on JavaScript.",
+      'Strongly typed programming language that builds on JavaScript.',
     logo: BookmarkIcon,
-    docs: "https://www.typescriptlang.org/",
+    docs: 'https://www.typescriptlang.org/',
   },
   {
-    name: "Tailwind with JIT",
-    description: "A utility-first CSS framework packed with classes.",
+    name: 'Tailwind with JIT',
+    description: 'A utility-first CSS framework packed with classes.',
     logo: PhotoIcon,
-    docs: "https://tailwindcss.com/",
+    docs: 'https://tailwindcss.com/',
   },
   {
-    name: "Jest",
-    description: "Testing Framework with a focus on simplicity.",
+    name: 'Jest',
+    description: 'Testing Framework with a focus on simplicity.',
     logo: QuestionMarkCircleIcon,
-    docs: "https://jestjs.io/",
+    docs: 'https://jestjs.io/',
   },
   {
-    name: "CSS Modules",
+    name: 'CSS Modules',
     description:
-      "CSS file in which all class names and animation names are scoped locally by default.",
+      'CSS file in which all class names and animation names are scoped locally by default.',
     logo: LockClosedIcon,
-    docs: "https://github.com/css-modules/css-modules",
+    docs: 'https://github.com/css-modules/css-modules',
   },
   {
-    name: "ESLint",
-    description: "Find and fix problems in your JavaScript code.",
+    name: 'ESLint',
+    description: 'Find and fix problems in your JavaScript code.',
     logo: BeakerIcon,
-    docs: "https://eslint.org/",
+    docs: 'https://eslint.org/',
   },
   {
-    name: "Prettier",
-    description: "An opinionated code formatter.",
+    name: 'Prettier',
+    description: 'An opinionated code formatter.',
     logo: Bars3Icon,
-    docs: "https://prettier.io/",
+    docs: 'https://prettier.io/',
   },
   {
-    name: "Husky",
+    name: 'Husky',
     description:
-      "Lint your commit messages, run tests, lint code, etc... when you commit or push.",
+      'Lint your commit messages, run tests, lint code, etc... when you commit or push.',
     logo: CakeIcon,
-    docs: "https://github.com/typicode/husky",
+    docs: 'https://github.com/typicode/husky',
   },
   {
-    name: "Commit-lint",
-    description: "Helps your team adhering to a commit convention.",
+    name: 'Commit-lint',
+    description: 'Helps your team adhering to a commit convention.',
     logo: FilmIcon,
-    docs: "https://github.com/conventional-changelog/commitlint",
+    docs: 'https://github.com/conventional-changelog/commitlint',
   },
   {
-    name: "Atomic design",
+    name: 'Atomic design',
     description:
-      "We’re not designing pages, we’re designing systems of components.",
+      'We’re not designing pages, we’re designing systems of components.',
     logo: PhoneXMarkIcon,
-    docs: "https://bradfrost.com/blog/post/atomic-web-design/",
+    docs: 'https://bradfrost.com/blog/post/atomic-web-design/',
   },
   {
-    name: "Absolute imports",
+    name: 'Absolute imports',
     description:
-      "Import resource using its full path from the project’s src folder.",
+      'Import resource using its full path from the project’s src folder.',
     logo: ChevronDownIcon,
-    docs: "https://github.com/vitejs/vite/issues/88#issuecomment-762415200",
+    docs: 'https://github.com/vitejs/vite/issues/88#issuecomment-762415200',
   },
-];
+]
 
 const App = (): JSX.Element => {
   return (
     <main className={styles.main}>
       <header className={styles.header}>
         <h3 className={styles.headerTopTitle}>
-          <span className={styles.headerTopTitleVital}>Vital</span> @ Vite
-          Template
+          <span className={styles.headerTopTitleVital}>Forum</span> @Neil
         </h3>
-        <h1 className={styles.headerTitle}>React + TypeScript + Tailwind</h1>
-        <p className={styles.headerDescription}>
-          Bootstrap your web projects faster than ever. Comes with:{" "}
-          <code className={styles.headerDescriptionCode}>CSS-Modules</code>,{" "}
-          <code className={styles.headerDescriptionCode}>Jest</code>,{" "}
-          <code className={styles.headerDescriptionCode}>Husky</code>,{" "}
-          <code className={styles.headerDescriptionCode}>Commit-lint</code>,{" "}
-          <code className={styles.headerDescriptionCode}>ESLint</code>,{" "}
-          <code className={styles.headerDescriptionCode}>Prettier</code> and{" "}
+        <h1 className={styles.headerTitle}>Forum project</h1>
+        {/* unused stuff */}
+        {/* <p className={styles.headerDescription}>
+          Bootstrap your web projects faster than ever. Comes with:{' '}
+          <code className={styles.headerDescriptionCode}>CSS-Modules</code>,{' '}
+          <code className={styles.headerDescriptionCode}>Jest</code>,{' '}
+          <code className={styles.headerDescriptionCode}>Husky</code>,{' '}
+          <code className={styles.headerDescriptionCode}>Commit-lint</code>,{' '}
+          <code className={styles.headerDescriptionCode}>ESLint</code>,{' '}
+          <code className={styles.headerDescriptionCode}>Prettier</code> and{' '}
           <code className={styles.headerDescriptionCode}>
             Atomic organization for components
           </code>
           . Configured and ready to go.
-        </p>
+        </p> */}
         <div className={styles.viteLogoContainer}>
           <Logos.Vite className={styles.viteLogo} />
         </div>
       </header>
       <section className={styles.copy}>
         <div className={styles.copyInner}>
-          <a href="https://github.com/jvidalv/vital">
-            <Button>Visit on Github</Button>
+          <a href="https://github.com/neilsen-ngoi">
+            <Button>Visit me on Github</Button>
           </a>
-          <CopyButton text="npx degit jvidalv/vital my-app" />
+          {/* copy feature unused */}
+          {/* <CopyButton text="npx degit jvidalv/vital my-app" /> */}
         </div>
       </section>
-      <section className={styles.features}>
+      {/* unused feature */}
+      {/* <section className={styles.features}>
         {features.map((props, index) => (
           <div
             key={index}
@@ -149,14 +151,14 @@ const App = (): JSX.Element => {
             />
           </div>
         ))}
-      </section>
+      </section> */}
       <footer className={styles.footer}>
-        <a href="https://github.com/jvidalv">
-          Josep Vidal @ {new Date().getFullYear()}
+        <a href="https://github.com/neilsen-ngoi">
+          Neil Wei @ {new Date().getFullYear()}
         </a>
       </footer>
     </main>
-  );
-};
+  )
+}
 
-export default App;
+export default App
